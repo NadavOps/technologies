5f5c553054e596cc770b16db698578836d56832a
NadavOps <72969918+NadavOps@users.noreply.github.com>
2022-02-15 20:59:28 +0200

    s3

--- aws_s3/aws_s3.sh
+++ aws_s3/aws_s3.sh
@@ -1,3 +1,40 @@
+# set AWS keys
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_DEFAULT_REGION=
+
+# set ENVS
+BUCKET_NAME=
+BUCKET_PATH=
+if [[ -z $BUCKET_PATH ]]; then
+    BUCKET_FULL_PATH=$BUCKET_NAME
+else
+    BUCKET_FULL_PATH=$BUCKET_NAME/$BUCKET_PATH
+fi
+
+# list bucket
+aws s3 ls s3://$BUCKET_FULL_PATH
+
+# copy object
+aws s3 cp ./filename s3://$BUCKET_FULL_PATH/filename
+
+# remove object
+aws s3 rm s3://$BUCKET_FULL_PATH/filename
+
+# get current bucket policy
+aws s3api get-bucket-policy --bucket $BUCKET_NAME | jq .
+
+# get bucket policy
+aws s3api put-bucket-policy --bucket $BUCKET_NAME --policy file://policy_example.json 
+
+# delete bucket policy
+aws s3api delete-bucket-policy --bucket $BUCKET_NAME
+
+
+#jms try
+aws iam list-users --max-items 20 --query 'Users[?CreateDate<=`2021-12-16`] | [?UserName=~`user_name`]'
+
+
 ## Average file size in S3 folder
 BUCKET_NAME=
 BUCKET_FOLDER_PATH=
