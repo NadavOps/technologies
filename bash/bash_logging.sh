4d32e3b9cc07614c813aefb0d71bfc7c27889ded
NadavOps <72969918+NadavOps@users.noreply.github.com>
2022-02-25 18:55:43 +0200

    bash_logging

--- bash/bash_logging.sh
+++ bash/bash_logging.sh
@@ -2,7 +2,8 @@
 bash_logging() {
     local default_color msg_severity msg_severity_color msg_content msg_content_color allowed_severity
     default_color='\033[0m'
-    msg_severity=$(echo "$1" | sed 's/[a-z]/\U&/g')
+    msg_severity=$(echo "$2" | sed 's/[a-z]/\U&/g')
+    msg_severity="${msg_severity:-INFO}"
     LOGGING_ALLOWED_SEVERITY="${LOGGING_ALLOWED_SEVERITY:-INFO}"
     allowed_severity=$(echo "$LOGGING_ALLOWED_SEVERITY" | sed 's/[A-Z]/\L&/g')
     case $msg_severity in
@@ -33,18 +34,18 @@ bash_logging() {
         $allowed_severity != "warn" && $allowed_severity != "error" ]]; then
             return 0
         fi
-        msg_severity_color="\033[0;31m"
-        msg_content_color="\033[1;31m"
+        msg_severity_color="\033[1;31m"
+        msg_content_color="\033[1;91m"
         ;;
     *)
         msg_severity_color="\033[0;31m"
         msg_content_color="\033[1;31m"
 printf """${msg_severity_color}\"bash_logging\" failed.
-${msg_content_color}Parameter #1: \"$msg_severity\" needs to be one of: \"DEBUG\", \"INFO\", \"WARN\", \"ERROR\"${default_color}
-"""
+${msg_content_color}Parameter #2: \"$msg_severity\" needs to be one of: \"DEBUG\", \"INFO\", \"WARN\", \"ERROR\"${default_color}
+""" >&2
         return 1
         ;;
     esac
-    msg_content="$2"
+    msg_content="$1"
     printf "${msg_severity_color}$(date -u "+%d.%m.%Y %H:%M:%S %Z") $msg_severity:${msg_content_color} $msg_content ${default_color}\n"
 }
