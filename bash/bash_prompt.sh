546d22086e73d348287f58032f3b7b4339172c6b
NadavOps <72969918+NadavOps@users.noreply.github.com>
2022-02-25 18:08:01 +0200

    bash prompt

--- bash/bash_prompt.sh
+++ bash/bash_prompt.sh
@@ -1,22 +1,19 @@
-export PROMPT_COMMAND=prompt_command
-
 prompt_command() {
     local EXIT="$?"
-    # Colours
-    local NC="\033[0m"; local cyan="\033[38;5;6m"; local lime='\033[38;5;118m'; local green='\033[38;5;2m'; local red='\033[38;5;1m'; local purplish='\033[38;5;12m'
-    # Choose colour for ret code
-    if [[ $EXIT != 0 ]]; then local ret_color=$red; else local ret_color=$cyan; fi
-    ### VOptional variables to use in PS1
-    # local USED_SHELL=$(ps -o comm= -p $$ | rev | cut -d "/" -f1 | rev)
-    # local username_and_machine="$USER@$(echo $HOSTNAME | cut -d "." -f1)"
-    # Define PS1
-    PS1="\[$cyan\][\[$ret_color\]${EXIT}\[$cyan\]]\[$cyan\] \[$purplish\]($(get_cur_time))\[$cyan\]$(parse_git_branch) \[$lime\]\w\[$NC\]$"
+    local default="\033[0m"; local cyan="\033[38;5;6m"; local lime='\033[38;5;118m';
+    local green='\033[38;5;2m'; local red='\033[38;5;1m'; local purplish='\033[38;5;12m'
+    # Choose colour for exit status code
+    if [[ $EXIT != 0 ]]; then local exit_color=$red; else local exit_color=$cyan; fi
+    # Prompt Line
+    PS1="${cyan}[${exit_color}$EXIT${cyan}] ${purplish}($(get_current_time)) ${lime}\w\n${cyan}$(parse_git_branch)${default}%"
 }
 
 parse_git_branch() {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+    git rev-parse --git-dir &> /dev/null && echo "($(git rev-parse --abbrev-ref HEAD)) "
 }
 
-get_cur_time() {
+get_current_time() {
     date +"%T"
 }
+
+export PROMPT_COMMAND=prompt_command
