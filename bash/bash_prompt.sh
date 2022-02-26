5686e450556e251385209d73252a58d368570fe5
NadavOps <72969918+NadavOps@users.noreply.github.com>
2022-02-26 21:23:09 +0200

    bash_prompt

--- bash/bash_prompt.sh
+++ bash/bash_prompt.sh
@@ -1,11 +1,16 @@
 prompt_command() {
-    local EXIT="$?"
+    local exit_code="$?"
     local default="\033[0m"; local cyan="\033[38;5;6m"; local lime='\033[38;5;118m';
     local green='\033[38;5;2m'; local red='\033[38;5;1m'; local purplish='\033[38;5;12m'
     # Choose colour for exit status code
-    if [[ $EXIT != 0 ]]; then local exit_color=$red; else local exit_color=$cyan; fi
+    if [[ $exit_code != 0 ]]; then local exit_color=$red; else local exit_color=$cyan; fi
     # Prompt Line
-    PS1="${cyan}[${exit_color}$EXIT${cyan}] ${purplish}($(get_current_time)) ${lime}\w\n${cyan}$(parse_git_branch)${default}%"
+    ps_exit_code="\[$cyan\][\[$exit_color\]${exit_code}\[$cyan\]]"
+    ps_time="\[$purplish\]($(get_current_time))"
+    ps_git_branch="\[$cyan\]$(parse_git_branch)"
+    ps_path="\[$lime\]\w"
+    ps_input="\n\[$default\]% "
+    PS1="$ps_exit_code $ps_time $ps_git_branch$ps_path$ps_input"
 }
 
 parse_git_branch() {
