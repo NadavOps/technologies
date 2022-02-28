f9e3eb9790f785b385d5ce8c4f697505efba1061
NadavOps <72969918+NadavOps@users.noreply.github.com>
2022-02-28 20:40:16 +0200

    bash_init_functions

--- bash/bash_init_functions.sh
+++ bash/bash_init_functions.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
-                    # function_name---local_path---remote_git_path
-ESSENTIAL_FUNCTIONS=( "bash_logging---bash_logging.sh---https://raw.githubusercontent.com/NadavOps/technologies/main/bash/bash_logging.sh" )
+                    # local_path---remote_git_path
+ESSENTIAL_FUNCTIONS=( "bash_logging.sh---https://raw.githubusercontent.com/NadavOps/technologies/main/bash/bash_logging.sh" )
 
+functions_counter=0
 for item in "${ESSENTIAL_FUNCTIONS[@]}" ; do
-    function_name=$(echo "$item" | awk -F "---" '{print $1}')
-    function_file_path=$(echo "$item" | awk -F "---" '{print $2}')
-    function_git_url=$(echo "$item" | awk -F "---" '{print $3}')
+    function_file_path=$(echo "$item" | awk -F "---" '{print $1}')
+    function_git_url=$(echo "$item" | awk -F "---" '{print $2}')
     if [[ -f $function_file_path ]]; then
+        set -a
         source $function_file_path
-        export -f $function_name
+        set +a
     else
-        source /dev/stdin <<< "$(curl --insecure $function_git_url)"
-        export -f $function_name
+        echo "WARN: \"$function_file_path\" was not found. Trying to export from git: $function_git_url"
+        set -a
+        source /dev/stdin <<< "$(curl -s --insecure $function_git_url)"
+        set +a
     fi
 
-    if [[ $(type -t $function_name) != function ]]; then
-        echo "\"$function_name\" function was not found. export the function and re-try" >&2
-        exit 1
+    functions_amount=$(declare -F | wc -l)
+
+    if [[ $functions_amount -le $functions_counter ]]; then
+        echo """ERROR: Something went wrong. functions were not exported from: \"$function_file_path\".
+       Managed to export $functions_amount function/s overall.
+       Terminating.""" >&2
+    exit 1
     fi
+
+    functions_counter=$(( ++$functions_amount ))
 done
