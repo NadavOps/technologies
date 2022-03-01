821df38ec18320046166c5cc431b8171c16baaf2
NadavOps <72969918+NadavOps@users.noreply.github.com>
2022-03-01 20:44:03 +0200

    bash_install_packages.sh

--- bash/bash_install_packages.sh
+++ bash/bash_install_packages.sh
@@ -7,36 +7,47 @@ verify_custom_functions_exists() {
             echo "\"$function_name\" function was not found. export the function and re-try" >&2
             exit 1
         fi
-    fi
+    done
 }
 
 verify_linux_package_manager() {
     if [[ $(which apt) ]]; then
-        bash_logging "\"apt\" package manager was found" DEBUG
+        bash_logging INFO "\"apt\" package manager was found"
         return 0
     else
-        bash_logging "The supported package manager for Linux is \"apt\" only, which was not found. terminating" ERROR
+        bash_logging ERROR "The supported package manager for Linux is \"apt\" only, which was not found. terminating"
         return 1
     fi
 }
 
 verify_mac_package_manager() {
     if [[ $(which brew) ]]; then
-        bash_logging "\"brew\" package manager was found" DEBUG
+        bash_logging INFO "\"brew\" package manager was found"
         return 0
     else
-        bash_logging "\"brew\" package manager was not found. Installing" DEBUG
+        bash_logging DEBUG "\"brew\" package manager was not found. Installing"
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     fi   
+}
+
+verify_parameters() {
+    local all_parameters
+    all_parameters=("$@")
+
+    declare -p all_parameters 2> /dev/null | grep -q '^declare \-a' \
+    || bash_logging ERROR "Something is not right with the parameters: \"${all_parameters[@]}\""
+
+    [[ ! -z ${all_parameters[@]} ]] && return 0 \
+    || bash_logging ERROR "No arguments were supplied: \"${all_parameters[@]}\""
 }
 
 verify_linux_package() {
     local package_name
     package_name="$1"
-    bash_logging "Verify linux package \"$package_name\"" DEBUG
+    bash_logging DEBUG "Verify linux package \"$package_name\""
     dpkg -s $package_name &> /dev/null && \
-    (bash_logging "Package: \"$package_name\" already installed" DEBUG && return 0) || \
-    (bash_logging "Package: \"$package_name\" is not installed" DEBUG && return 1)
+    (bash_logging DEBUG "Package: \"$package_name\" already installed" && return 0) || \
+    (bash_logging DEBUG "Package: \"$package_name\" is not installed" && return 1)
 }
 
 install_repository() {
@@ -45,69 +56,74 @@ install_repository() {
     install_repo="$2"
     gpg_key_url="$3"
     if [[ $gpg_key_url ]]; then
-        bash_logging "Adding gpg_key: $gpg_key_url and repo: $install_repo" DEBUG
+        bash_logging DEBUG "Adding gpg_key: $gpg_key_url and repo: $install_repo"
         sudo curl -s "$gpg_key_url" | sudo apt-key add - || return 1
         sudo echo "deb $install_repo" | sudo tee "/etc/apt/sources.list.d/$package_name.list"
         sudo apt-get update -y && \
-        (bash_logging "Repository for package: \"$package_name\" updated" DEBUG && return 0) || \
-        (bash_logging "Repository for package: \"$package_name\" failed. terminating" ERROR && return 1)
+        (bash_logging DEBUG "Repository for package: \"$package_name\" updated" && return 0) || \
+        (bash_logging ERROR "Repository for package: \"$package_name\" failed. terminating" && return 1)
     # This was for when I thought I can install without gpg, might be usefull
     # elif [[ $install_repo ]]; then
-    #     bash_logging "Adding repo: $install_repo without gpg_key" DEBUG
+    #     bash_logging DEBUG "Adding repo: $install_repo without gpg_key"
     #     echo "deb $install_repo" | sudo tee "/etc/apt/sources.list.d/$package_name.list"
     #     sudo apt-get update -y && \
-    #     (bash_logging "Repository for package: \"$package_name\" updated" DEBUG && return 0) || \
-    #     (bash_logging "Repository for package: \"$package_name\" failed. terminating" ERROR && exit 1)
+    #     (bash_logging DEBUG "Repository for package: \"$package_name\" updated" && return 0) || \
+    #     (bash_logging ERROR "Repository for package: \"$package_name\" failed. terminating" && exit 1)
     else
-        bash_logging "No gpg_key: $gpg_key_url or repo: $install_repo were supplied, continue to installation" DEBUG
+        bash_logging DEBUG "No gpg_key: $gpg_key_url or repo: $install_repo were supplied, continue to installation"
     fi
 }
 
 install_linux_package() {
     local package_name
     package_name="$1"
-    bash_logging "Installing linux package: \"$package_name\"" DEBUG
+    bash_logging DEBUG "Installing linux package: \"$package_name\""
     sudo apt-get install -y "$package_name" && return 0 || return 1  
 }
 
-install_all_linux_packages() {
-    local package_name cli_package_item install_repo gpg_key_url
+install_linux_packages_list() {
+    local packages_list package_item package_name package_type install_repo gpg_key_url
 
-    bash_logging "Updating apt" DEBUG
+    packages_list=("$@")
+
+    verify_parameters "${packages_list[@]}"
+    
+    bash_logging DEBUG "Updating apt"
     sudo apt-get update -y
 
-    for package_name in "${PACKAGES_PREREQUISITE[@]}" ; do
+    for package_item in "${packages_list[@]}" ; do
+        package_name=$(echo "$package_item" | awk -F "---" '{print $1}')
+        package_type=$(echo "$package_item" | awk -F "---" '{print $2}' | tr [[:lower:]] [[:upper:]])
+        [[ "$package_type" == "GUI" ]] && bash_logging WARN "This script doesn't support GUI installs for linux. \"package_type\" is: $package_type" && continue
         verify_linux_package "$package_name" && continue
-        install_linux_package "$package_name"
-    done
-
-    for cli_package_item in "${PACKAGES_CLI[@]}" ; do
-        package_name=$(echo "$cli_package_item" | awk -F "---" '{print $1}')
-        verify_linux_package "$package_name" && continue
-        install_repo=$(echo "$cli_package_item" | awk -F "---" '{print $2}')
-        gpg_key_url=$(echo "$cli_package_item" | awk -F "---" '{print $3}')
-        install_repository "$package_name" "$install_repo" "$gpg_key_url" || exit 1
-        install_linux_package "$package_name"
+        install_repo=$(echo "$package_item" | awk -F "---" '{print $3}')
+        gpg_key_url=$(echo "$package_item" | awk -F "---" '{print $4}')
+        bash_logging "summary" INFO
+        bash_logging "$package_name" INFO
+        bash_logging "$install_repo" INFO
+        bash_logging "$gpg_key_url" INFO
+        # install_repository "$package_name" "$install_repo" "$gpg_key_url" || exit 1
+        # install_linux_package "$package_name"
     done
 }
 
 verify_mac_package() {
     local package_name package_type verify_command brew_flag
     package_name="$1"
-    package_type="$2"
+    package_type=$( echo "$2" | tr [[:lower:]] [[:upper:]] )
     if [[ $package_type == "CLI" ]]; then
-        bash_logging "Verify CLI mac package \"$package_name\"" DEBUG
+        bash_logging DEBUG "Verify CLI mac package \"$package_name\""
         brew_flag="--formula"
     elif [[ $package_type == "GUI" ]]; then
-        bash_logging "Verify GUI mac package \"$package_name\"" DEBUG
+        bash_logging DEBUG "Verify GUI mac package \"$package_name\""
         brew_flag="--cask"
     else
-        bash_logging "Verifying mac package failed. package_name: $package_name, package_type: $package_type. terminating" ERROR
+        bash_logging ERROR "Verifying mac package: \"$package_name\" failed. package_type: \"$package_type\" is not correct. terminating"
         exit 1
     fi
     verify_command="brew list $brew_flag | grep \"$package_name\$\" &> /dev/null && \
-                    (bash_logging \"Package: $package_name already installed\" DEBUG && return 0) || \
-                    (bash_logging \"Package: $package_name is not installed\" DEBUG && return 1)"
+                    (bash_logging INFO \"Package: $package_name already installed\" && return 0) || \
+                    (bash_logging WARN \"Package: $package_name is not installed\" && return 1)"
     eval $verify_command
 }
 
@@ -116,77 +132,74 @@ install_mac_package() {
     package_name="$1"
     package_type="$2"
     if [[ $package_type == "CLI" ]]; then
-        bash_logging "Install CLI mac package \"$package_name\"" DEBUG
+        bash_logging DEBUG "Install CLI mac package \"$package_name\""
         brew_flag=""
     elif [[ $package_type == "GUI" ]]; then
-        bash_logging "Install GUI mac package \"$package_name\"" DEBUG
+        bash_logging DEBUG "Install GUI mac package \"$package_name\""
         brew_flag=" --cask"
     else
-        bash_logging "Installing mac package failed. package_name: \"$package_name\", package_type: \"$package_type\". terminating" ERROR
+        bash_logging ERROR "Installing mac package failed. package_name: \"$package_name\", package_type: \"$package_type\". terminating"
         exit 1
     fi
-    verify_command="sudo brew install$brew_flag $package_type"
-    eval $verify_command
+    verify_command="brew install$brew_flag $package_name"
+    bash_logging DEBUG "$verify_command"
+    eval $verify_command && return 0
+    bash_logging ERROR "Installing mac package failed. package_name: \"$package_name\", package_type: \"$package_type\". terminating" && exit 1
 }
 
-install_all_mac_packages() {
-    local package_item package_name install_repo gpg_key_url
+install_mac_packages_list() {
+    local packages_list package_item package_name package_type
 
-    for package_item in "${PACKAGES_CLI[@]}" ; do
+    packages_list=("$@")
+
+    verify_parameters "${packages_list[@]}"
+
+    for package_item in "${packages_list[@]}" ; do
         package_name=$(echo "$package_item" | awk -F "---" '{print $1}')
-        verify_mac_package "$package_name" "CLI" && continue
-        install_mac_package "$package_name" "CLI"
-    done
-
-    for package_item in "${PACKAGES_MAC[@]}" ; do
-        package_name=$(echo "$package_item" | awk -F "---" '{print $1}')
-        verify_mac_package "$package_name" "CLI" && continue
-        install_mac_package "$package_name" "CLI"
-    done
-
-    for package_name in "${PACKAGES_GUI[@]}" ; do
-        verify_mac_package "$package_name" "GUI" && continue
-        install_mac_package "$package_name" "GUI"
+        package_type=$(echo "$package_item" | awk -F "---" '{print $2}' | tr [[:lower:]] [[:upper:]])
+        verify_mac_package "$package_name" "$package_type" && continue
+        install_mac_package "$package_name" "$package_type"
     done
 }
 
 main() {
     local os_type
     verify_custom_functions_exists
-    os_type=$(uname | sed 's/[[:alnum:]]/\L&/g')
+    os_type=$(uname | tr [[:upper:]] [[:lower:]])
     if [[ $os_type == *linux* ]]; then
-        bash_logging "we in linux" DEBUG
+        bash_logging DEBUG "we in linux"
         verify_linux_package_manager
-        install_all_linux_packages
+        install_linux_packages_list "${PACKAGES_LIST[@]}"
     elif [[ $os_type == *darwin* ]]; then
-        bash_logging "we in mac" DEBUG
+        bash_logging DEBUG "we in mac"
         verify_mac_package_manager
-        install_all_mac_packages
+        install_mac_packages_list "${PACKAGES_LIST[@]}"
     else
-        bash_logging "what is this OS? (os_type is $os_type)" ERROR
+        bash_logging ERROR "what is this OS? (os_type is $os_type)"
+        return 1
     fi
 }
 
-
-PACKAGES_PREREQUISITE=( "apt-transport-https"
-                        "ca-certificates"
-                        "curl" )
-             # package_name---install_repo---gpg_key
-PACKAGES_CLI=( "kubectl---https://apt.kubernetes.io/ kubernetes-xenial main---https://packages.cloud.google.com/apt/doc/apt-key.gpg"
-                "helm---https://baltocdn.com/helm/stable/debian/ all main---https://baltocdn.com/helm/signing.asc"
-                "jq"
-                "shellcheck"
-                "mysql-client" )
-             # unsupported for linux ATM
-             # install_name---filesystem_name
-PACKAGES_GUI=( "microsoft-edge"
-               "google-chrome"
-               "firefox"
-               "visual-studio-code"
-               "iterm2" )
-PACKAGES_MAC=( "lima"
-               "docker" )
-
 source bash_init_functions.sh
-
+PACKAGES_LIST=("$@")
 main
+
+
+# PACKAGES_PREREQUISITE=( "apt-transport-https"
+#                         "ca-certificates"
+#                         "curl" )
+#              # package_name---install_repo---gpg_key
+# PACKAGES_CLI=( "kubectl---https://apt.kubernetes.io/ kubernetes-xenial main---https://packages.cloud.google.com/apt/doc/apt-key.gpg"
+#                 "helm---https://baltocdn.com/helm/stable/debian/ all main---https://baltocdn.com/helm/signing.asc"
+#                 "jq"
+#                 "shellcheck"
+#                 "mysql-client" )
+#              # unsupported for linux ATM
+#              # install_name---filesystem_name
+# PACKAGES_GUI=( "microsoft-edge"
+#                "google-chrome"
+#                "firefox"
+#                "visual-studio-code"
+#                "iterm2" )
+# PACKAGES_MAC=( "lima"
+#                "docker" )
