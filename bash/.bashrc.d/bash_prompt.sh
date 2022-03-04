ff0b486ccf21be1990dde1836d7f78ac016754c1
NadavOps <72969918+NadavOps@users.noreply.github.com>
2022-03-04 14:12:52 +0200

    bash_prompt.sh

--- bash/.bashrc.d/bash_prompt.sh
+++ bash/.bashrc.d/bash_prompt.sh
@@ -1,4 +1,4 @@
-prompt_command() {
+bash_prompt() {
     local exit_code="$?"
     local default="\033[0m"; local cyan="\033[38;5;6m"; local lime='\033[38;5;118m';
     local green='\033[38;5;2m'; local red='\033[38;5;1m'; local purplish='\033[38;5;12m'
@@ -21,4 +21,4 @@ get_current_time() {
     date +"%T"
 }
 
-export PROMPT_COMMAND=prompt_command
+export PROMPT_COMMAND=bash_prompt
