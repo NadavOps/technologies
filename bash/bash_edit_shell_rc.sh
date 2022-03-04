bb2cd91043a25337dcf76cd504deed84307bd55a
NadavOps <72969918+NadavOps@users.noreply.github.com>
2022-03-04 14:14:07 +0200

    bash_edit_shell_rc.sh

---
+++ bash/bash_edit_shell_rc.sh
@@ -0,0 +1,38 @@
+update_rc_file() {
+    local current_rc_file archive_directory input_shell_rc_file
+    current_rc_file="$1"
+    archive_directory="$2"
+    input_shell_rc_file="$2"
+    bash_logging DEBUG "Editing \"$current_rc_file\". (os_type: \"$os_type\")"
+    archive_file "$current_rc_file" "$archive_directory" || return 1
+    cp "$input_shell_rc_file" "$current_rc_file" && \
+    bash_logging INFO "Copied \"$input_shell_rc_file\" into the running rc file: \"$current_rc_file\"" || \
+    bash_logging ERROR "Failed to copy \"$input_shell_rc_file\" into the running rc file: \"$current_rc_file\""
+}
+
+bash_edit_shell_rc() {
+    verify_imported_functions_exists "bash_logging" "verify_array" "archive_file"
+    bash_logging DEBUG "Running from $0"
+    local input_shell_rc_file input_scripts_directories os_type archive_directory current_rc_file
+    input_shell_rc_file="$1"
+    verify_file "$input_shell_rc_file" || return 1
+    shift 1
+    input_scripts_directories=("$@")
+    archive_directory="$HOME/archive"
+    mkdir -p "$archive_directory"
+    os_type=$(uname | tr [[:upper:]] [[:lower:]])
+    if [[ $os_type == *linux* || $os_type == *darwin* ]]; then
+        current_rc_file="$HOME/.bashrc"
+        update_rc_file "$current_rc_file" "$archive_directory" "$input_shell_rc_file" || exit 1
+    fi
+    if [[ $os_type == *darwin* ]]; then
+        current_rc_file="$HOME/.zshrc"
+        update_rc_file "$current_rc_file" "$archive_directory" "$input_shell_rc_file" || exit 1
+    fi
+    if [[ $os_type != *linux* && $os_type != *darwin* ]]; then
+        bash_logging ERROR "what is this OS? (os_type is $os_type)"
+        exit 1
+    fi
+}
+
+## add bash_prompt_source
